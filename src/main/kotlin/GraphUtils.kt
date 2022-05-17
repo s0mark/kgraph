e2749ec47132d0ce0cb7d3a31c95793bebb328f7
s0mark <somorjai.mark@icloud.com>
2022-05-17 22:04:47 +0200

    SFS with list, BFS, DFS

--- src/main/kotlin/GraphUtils.kt
+++ src/main/kotlin/GraphUtils.kt
@@ -1,27 +1,40 @@
-enum class SearchNodeState {
-    UNSEEN,
-    DISCOVERED,
-    VISITED,
-}
+open class SFSIterator<T, N : Node<T>>(graph: Graph<T, out N>, start: N? = null) : Iterator<N> {
+    private val visitMap: MutableMap<N, Boolean> = graph.nodes.associateWith { false }.toMutableMap()
+    protected val toVisit: MutableList<N> = mutableListOf(start ?: graph.nodes.first())
 
-open class SFSIterator<T, N : Node<T>>(private val graph: Graph<T, out N>, start: N? = null) : Iterator<N> {
-    protected val visitMap: MutableMap<N, SearchNodeState> = mutableMapOf<N, SearchNodeState>().apply {
-        graph.nodes.forEach { put(it, SearchNodeState.UNSEEN) }
-        put(start ?: graph.nodes.first(), SearchNodeState.DISCOVERED)
+    private fun N.discover() {
+        visitMap[this] = true
+        for (neighbor in neighbors) {
+            if (!visitMap.getValue(neighbor as N)) toVisit.add(neighbor)
+        }
     }
 
-    open fun nextToVisit(): N {
-        return visitMap.filter { (_, state) -> state == SearchNodeState.DISCOVERED }.keys.random()
-    }
+    open fun nextToVisit(): N = toVisit.random()
 
-    override fun hasNext(): Boolean = visitMap.any { (_, state) -> state != SearchNodeState.VISITED }
+    override fun hasNext(): Boolean = !visitMap.all { (_, visited) -> visited }
 
     override fun next(): N {
-        val visiting = nextToVisit()
-        visiting.neighbors.forEach { neighbor ->
-            if (visitMap.getValue(neighbor as N) == SearchNodeState.UNSEEN) visitMap[neighbor] = SearchNodeState.DISCOVERED
-        }
-        visitMap[visiting] = SearchNodeState.VISITED
+        val visiting = if (toVisit.isNotEmpty())
+            nextToVisit()
+        else
+            visitMap.filter { (_, visited) -> !visited }.keys.random()
+        visiting.discover()
         return visiting
     }
 }
+
+class BFSIterator<T, N : Node<T>>(graph: Graph<T, out N>, start: N?) : SFSIterator<T, N>(graph, start) {
+    override fun nextToVisit(): N {
+        return toVisit.first()
+    }
+}
+
+fun<T, N : Node<T>> Graph<T, out N>.bfs(start: N? = null): Iterator<N> = BFSIterator(this, start)
+
+class DFSIterator<T, N : Node<T>>(graph: Graph<T, out N>, start: N?) : SFSIterator<T, N>(graph, start) {
+    override fun nextToVisit(): N {
+        return toVisit.last()
+    }
+}
+
+fun<T, N : Node<T>> Graph<T, out N>.dfs(start: N? = null): Iterator<N> = DFSIterator(this, start)
