7fe297cc3fab5c5d93eb9676790c3121dac086d2
s0mark <somorjai.mark@icloud.com>
2022-05-20 23:24:52 +0200

    max flow in directed graphs

--- src/main/kotlin/utils/Properties.kt
+++ src/main/kotlin/utils/Properties.kt
@@ -3,6 +3,8 @@ package utils
 import DirectedGraph
 import Node
 import UndirectedGraph
+import WeightedDirectedGraph
+import WeightedNode
 
 val<T, N : Node<T, N>> DirectedGraph<T, N>.isAcyclic: Boolean
     get() {
@@ -27,6 +29,102 @@ val<T, N : Node<T, N>> DirectedGraph<T, N>.isAcyclic: Boolean
         }
         return acyclic
     }
+
+private class CorrectorGraph<T, N : Node<T, N>>(graph: DirectedGraph<T, N>, from: N, to: N, val edgeCapacity: (N, N) -> Double) {
+    private val corrector = WeightedDirectedGraph<T>()
+    val nodeMap = graph.nodes.associateWith { corrector.addNodeOf(it.value) }
+    fun N.c() = nodeMap.getValue(this)
+    val source = from.c()
+    val target = to.c()
+    init {
+        graph.forEachEdge { node, neighbor ->
+            corrector.addEdge(neighbor.c(), node.c(), 0.0)
+            corrector.addEdge(node.c(), neighbor.c(), edgeCapacity(node, neighbor))
+        }
+    }
+
+    var flow: Double = 0.0
+        private set
+
+    fun ArrayDeque<WeightedNode<T>>.forEdges(action: (WeightedNode<T>, WeightedNode<T>, Double) -> Unit) {
+        this.reduce { node, neighbor ->
+            action(node, neighbor, node.getWeight(neighbor)!!)
+            neighbor
+        }
+    }
+
+    fun findCorrectingPath(): ArrayDeque<WeightedNode<T>> {
+        val visitMap = corrector.nodes.associateWith { false }.toMutableMap()
+        val path: ArrayDeque<WeightedNode<T>> = ArrayDeque()
+        fun WeightedNode<T>.discover(): Boolean {
+            visitMap[this] = true
+            path.add(this)
+            if (this === target) return true
+            neighbors.forEach { neighbor ->
+                if (!visitMap.getValue(neighbor) && getWeight(neighbor)!! > 0) {
+                    val routeFound = neighbor.discover()
+                    if (routeFound) return true
+                }
+            }
+            path.removeLast()
+            return false
+        }
+        source.discover()
+        return path
+    }
+
+    fun improveWithPath(correctingPath: ArrayDeque<WeightedNode<T>>) {
+        var correction = Double.POSITIVE_INFINITY
+        correctingPath.forEdges { _, _, remainingCapacity ->
+            if (remainingCapacity < correction) correction = remainingCapacity
+        }
+        correctingPath.forEdges { node, neighbor, remainingCapacity ->
+            corrector.apply {
+                val currentFlow = neighbor.getWeight(node)!!
+                removeEdge(node, neighbor).also { addEdge(node, neighbor, remainingCapacity - correction) }
+                removeEdge(neighbor, node).also { addEdge(neighbor, node, currentFlow + correction) }
+            }
+        }
+        flow += correction
+    }
+}
+
+fun<T, N : Node<T, N>> maxFlow(
+    graph: DirectedGraph<T, N>,
+    from: N,
+    to: N,
+    edgeCapacity: (N, N) -> Double = { _, _ -> 1.0 },
+): Double {
+    val corrector = CorrectorGraph(graph, from, to, edgeCapacity)
+    var correctingPath = corrector.findCorrectingPath()
+    while (correctingPath.size > 1) {
+        corrector.improveWithPath(correctingPath)
+        correctingPath = corrector.findCorrectingPath()
+    }
+    return corrector.flow
+}
+
+fun<T, N : Node<T, N>> maxFlow(
+    graph: DirectedGraph<T, N>,
+    edgeCapacity: (N, N) -> Double = { _, _ -> 1.0 },
+): Double {
+    return graph.nodes.maxOfOrNull { node ->
+        graph.nodes.maxOfOrNull { neighbor ->
+            if (node !== neighbor) maxFlow(graph, node, neighbor, edgeCapacity) else 0.0
+        } ?: 0.0
+    } ?: 0.0
+}
+
+fun<T, N : Node<T, N>> DirectedGraph<T, N>.maxFlow(from: N, to: N): Double = maxFlow(this, from, to)
+
+fun<T> WeightedDirectedGraph<T>.maxFlow(from: WeightedNode<T>, to: WeightedNode<T>): Double
+    = maxFlow(this, from, to) { node, neighbor -> node.getWeight(neighbor)!! }
+
+val<T, N : Node<T, N>> DirectedGraph<T, N>.maxFlow: Double
+    get() = maxFlow(this)
+
+val<T> WeightedDirectedGraph<T>.maxFlow: Double
+    get() = maxFlow(this) { node, neighbor -> node.getWeight(neighbor)!! }
 
 val<T, N : Node<T, N>> UndirectedGraph<T, N>.isConnected: Boolean
     get() {
--- src/test/kotlin/utilTests/PropertyTest.kt
+++ src/test/kotlin/utilTests/PropertyTest.kt
@@ -4,12 +4,16 @@ import DirectedMultiGraph
 import DirectedSimpleGraph
 import UndirectedMultiGraph
 import UndirectedSimpleGraph
+import WeightedDirectedGraph
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Test
 import utils.isAcyclic
 import utils.isConnected
+import utils.maxFlow
 
 class PropertyTest {
+    private val tolerance = 0.00001
 
     @Test
     fun `is simple undirected graph connected`() {
@@ -87,5 +91,55 @@ class PropertyTest {
         assertFalse(graph.isAcyclic)
         graph.removeNode(nodes[0])
         assert(graph.isAcyclic)
+    }
+
+    @Test
+    fun `max flow in unweighted graph`() {
+        val graph = DirectedSimpleGraph<Int>()
+        val nodes = (0..3).map { graph.addNodeOf(it) }
+        graph.addEdge(nodes[0], nodes[1])
+        graph.addEdge(nodes[0], nodes[2])
+        graph.addEdge(nodes[1], nodes[2])
+        graph.addEdge(nodes[1], nodes[3])
+        graph.addEdge(nodes[2], nodes[3])
+        graph.addEdge(nodes[3], nodes[0])
+        assertEquals(1.0, graph.maxFlow(nodes[0], nodes[1]), tolerance)
+        assertEquals(1.0, graph.maxFlow(nodes[1], nodes[0]), tolerance)
+        assertEquals(2.0, graph.maxFlow(nodes[0], nodes[2]), tolerance)
+        assertEquals(1.0, graph.maxFlow(nodes[2], nodes[0]), tolerance)
+        assertEquals(2.0, graph.maxFlow(nodes[0], nodes[3]), tolerance)
+        assertEquals(1.0, graph.maxFlow(nodes[3], nodes[0]), tolerance)
+        assertEquals(2.0, graph.maxFlow(nodes[1], nodes[2]), tolerance)
+        assertEquals(1.0, graph.maxFlow(nodes[2], nodes[1]), tolerance)
+        assertEquals(2.0, graph.maxFlow(nodes[1], nodes[3]), tolerance)
+        assertEquals(1.0, graph.maxFlow(nodes[3], nodes[1]), tolerance)
+        assertEquals(1.0, graph.maxFlow(nodes[2], nodes[3]), tolerance)
+        assertEquals(1.0, graph.maxFlow(nodes[3], nodes[2]), tolerance)
+        assertEquals(2.0, graph.maxFlow, tolerance)
+    }
+
+    @Test
+    fun `max flow in weighted graph`() {
+        val graph = WeightedDirectedGraph<Int>()
+        val nodes = (0..3).map { graph.addNodeOf(it) }
+        graph.addEdge(nodes[0], nodes[1], 3.0)
+        graph.addEdge(nodes[0], nodes[2], 6.0)
+        graph.addEdge(nodes[1], nodes[2], 10.0)
+        graph.addEdge(nodes[1], nodes[3], 7.0)
+        graph.addEdge(nodes[2], nodes[3], 9.0)
+        graph.addEdge(nodes[3], nodes[0], 8.0)
+        assertEquals(3.0, graph.maxFlow(nodes[0], nodes[1]), tolerance)
+        assertEquals(8.0, graph.maxFlow(nodes[1], nodes[0]), tolerance)
+        assertEquals(9.0, graph.maxFlow(nodes[0], nodes[2]), tolerance)
+        assertEquals(8.0, graph.maxFlow(nodes[2], nodes[0]), tolerance)
+        assertEquals(9.0, graph.maxFlow(nodes[0], nodes[3]), tolerance)
+        assertEquals(8.0, graph.maxFlow(nodes[3], nodes[0]), tolerance)
+        assertEquals(16.0, graph.maxFlow(nodes[1], nodes[2]), tolerance)
+        assertEquals(3.0, graph.maxFlow(nodes[2], nodes[1]), tolerance)
+        assertEquals(16.0, graph.maxFlow(nodes[1], nodes[3]), tolerance)
+        assertEquals(3.0, graph.maxFlow(nodes[3], nodes[1]), tolerance)
+        assertEquals(9.0, graph.maxFlow(nodes[2], nodes[3]), tolerance)
+        assertEquals(8.0, graph.maxFlow(nodes[3], nodes[2]), tolerance)
+        assertEquals(16.0, graph.maxFlow, tolerance)
     }
 }
